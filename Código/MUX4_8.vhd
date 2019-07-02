fef1aa481cfae706460caa774c79fefa41a6b800
GabrielCMoretto <gabrielmorettopvh@hotmail.com.br>
2019-07-01 21:38:05 -0300

    Update MUX4_8.vhd
    Aqui tinha que colocar aspas duplas nos bits

--- Código/MUX4_8.vhd
+++ Código/MUX4_8.vhd
@@ -10,17 +10,18 @@ port(
   D2: in STD_LOGIC_VECTOR(7 downto 0);
   D3: in STD_LOGIC_VECTOR(7 downto 0);
   S1: in STD_LOGIC_VECTOR(1 downto 0);
-  Y1: out STD_LOGIC_VECTOR(7 downto 0));
+  Y1: out STD_LOGIC_VECTOR(7 downto 0)
+  );
 
 end MUX4_8;
 
 architecture MUX of MUX4_8 is
 
 begin
-  Y1 <= D0 when S1 = '00' else
-        D1 when S1 = '01' else
-        D2 when S1 = '10' else
-        D3 when S1 = '11' else
-        00000000;
 
+  Y1 <= D0 when S1 = "00" else
+        D1 when S1 = "01" else
+        D2 when S1 = "10" else
+        D3 when S1 = "11" else
+        "00000000";
 end MUX;
